276b7317f670be78eab8e5067c025b143ada2eaa
syeshaChandhok <85556474+syeshaChandhok@users.noreply.github.com>
2021-09-17 17:14:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var first_name = "Shahrukh";
+var last_name = "Khan";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+var a = 98;
+var b = 15;                       
+var add = a+b;
+console.log(add);
+
+var subtract = a-b;
+console.log(subtract);
+
+var multiply = a*b;
+console.log(multiply);
+
+var divide = a/b;
+console.log(divide);
